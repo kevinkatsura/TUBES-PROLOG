84f3e029b7e08cf760096fdd7a98da4b0be9bddd
louis5md <louisriemen@gmail.com>
2020-11-23 20:26:47 +0700

    updown

--- updown.pl
+++ updown.pl
@@ -10,64 +10,105 @@
 :-dynamic(batasHP/1).
 job(swordsman).
 exp(0).
-attack(100).
-defense(80).
+attack(0).
+defense(0).
 level(1).
 gold(1000).
 health(1000).
 batasHP(1000).
 
 maxExp:-	job(swordsman),
+		level(5),!,
+		asserta(batasExp(99999)),
+		asserta(batasHP(1600)),
+		asserta(attack(175)),
+		asserta(defense(100)).
+maxExp:-	job(swordsman),
 		level(4),!,
 		asserta(batasExp(750)),
-		asserta(batasHP(1500)).
+		asserta(batasHP(1500)),
+		asserta(attack(165)),
+		asserta(defense(95)).
 maxExp:-	job(swordsman),
 		level(3),!,
 		asserta(batasExp(600)),
-		asserta(batasHP(1350)).
+		asserta(batasHP(1350)),
+		asserta(attack(150)),
+		asserta(defense(90)).
 maxExp:-	job(swordsman),
 		level(2),!,
 		asserta(batasExp(450)),
-		asserta(batasHP(1200)).
+		asserta(batasHP(1200)),
+		asserta(attack(130)),
+		asserta(defense(80)).
 maxExp:-	job(swordsman),
 		level(1),!,
 		asserta(batasExp(300)),
-		asserta(batasHP(1000)).
+		asserta(batasHP(1000)),
+		asserta(attack(100)),
+		asserta(defense(70)).
 
-
+maxExp:-	job(archer),
+		level(5),!,
+		asserta(batasExp(99999)),
+		asserta(batasHP(1450)),
+		asserta(attack(170)),
+		asserta(defense(90)).
 maxExp:-	job(archer),
 		level(4),!,
 		asserta(batasExp(600)),
-		asserta(batasHP(1300)).
+		asserta(batasHP(1300)),
+		asserta(attack(140)),
+		asserta(defense(80)).
 maxExp:-	job(archer),
 		level(3),!,
 		asserta(batasExp(500)),
-		asserta(batasHP(1250)).
+		asserta(batasHP(1250)),
+		asserta(attack(118)),
+		asserta(defense(80)).
 maxExp:-	job(archer),
 		level(2),!,
 		asserta(batasExp(400)),
-		asserta(batasHP(1100)).
+		asserta(batasHP(1100)),
+		asserta(attack(95)),
+		asserta(defense(70)).
 maxExp:-	job(archer),
 		level(1),!,
 		asserta(batasExp(250)),
-		asserta(batasHP(1000)).
+		asserta(batasHP(1000)),
+		asserta(attack(80)),
+		asserta(defense(70)).
 
+maxExp:-	job(sorcerer),
+		level(5),!,
+		asserta(batasExp(99999)),
+		asserta(batasHP(1450)),
+		asserta(attack(185)),
+		asserta(defense(80)).
 maxExp:-	job(sorcerer),
 		level(4),!,
 		asserta(batasExp(650)),
-		asserta(batasHP(1300)).
+		asserta(batasHP(1300)),
+		asserta(attack(145)),
+		asserta(defense(70)).
 maxExp:-	job(sorcerer),
 		level(3),!,
 		asserta(batasExp(500)),
-		asserta(batasHP(1200)).
+		asserta(batasHP(1200)),
+		asserta(attack(110)),
+		asserta(defense(60)).
 maxExp:-	job(sorcerer),
 		level(2),!,
 		asserta(batasExp(350)),
-		asserta(batasHP(1100)).
+		asserta(batasHP(1100)),
+		asserta(attack(85)),
+		asserta(defense(55)).
 maxExp:-	job(sorcerer),
 		level(1),!,
 		asserta(batasExp(250)),
-		asserta(batasHP(1000)).
+		asserta(batasHP(1000)),
+		asserta(attack(70)),
+		asserta(defense(50)).
 
 naikLevel:-	exp(Y1),!,
 		maxExp,
@@ -77,7 +118,10 @@ naikLevel:-	exp(Y1),!,
 		level(Z),
 		Z1 is Z+1,
 		asserta(level(Z1)),
-		asserta(exp(Y2)).
+		maxExp,
+		batasHP(R),
+		asserta(exp(Y2)),
+		asserta(health(R)).
 
 
 upExp(X):-	exp(Y),!,
@@ -104,7 +148,7 @@ downHealth(X):-	health(Y),
 		asserta(health(Y1)).
 
 
-status:-	write('Your Status'),nl,
+status:-	write('Your Status: '),nl,
 		maxExp,
 		job(Q),
 		level(S),
